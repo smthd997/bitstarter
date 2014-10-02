8246940e863110ef729c51c8b8e68a0411fa264c
Daniel Smith <smthd997@aol.com>
2014-10-02 13:38:23 +0000

    index.js 13i57iyu

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express();
 var fs = require('fs')
-var buf = fs.readFileSync("index.html")
+var buf = fs.readFileSync("./index.html","utf8")
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
