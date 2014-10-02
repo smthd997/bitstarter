152b2539a8a42ae3faa7b9c8e55392062e5974e1
Daniel Smith <smthd997@aol.com>
2014-10-02 13:19:30 +0000

    index.html 3

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express();
+var fs = require('fs')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync(index.html,null))
+  response.send(fs.readFileSync(index.html,encoding).toString())
 })
 
 app.listen(app.get('port'), function() {
