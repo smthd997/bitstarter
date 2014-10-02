809dbbc5f3be8cffeef06cee21588befd9072495
Daniel Smith <smthd997@aol.com>
2014-10-02 13:08:08 +0000

    trying to read from index.html

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Mr Aronson!')
+  response.send(buf.toString(fs.readFileSync(index.html))
 })
 
 app.listen(app.get('port'), function() {
