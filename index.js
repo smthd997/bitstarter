7123fa147bc11d69fce776aad5f6ebbb7c0f62f2
Daniel Smith <smthd997@aol.com>
2014-10-04 15:53:32 +0000

    fucking index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf8',0,buf.length)
+  response.send(buf)
 })
 
 app.listen(app.get('port'), function() {
